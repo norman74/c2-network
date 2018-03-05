eb483194565c560445f251fd5d8383219a13b709
norman74 <norman74@list.ru>
2018-03-05 17:43:53 +0500

    v0.1

---
+++ edittime.js
@@ -0,0 +1,61 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Network",
+		"id":			"networkAddon",
+		"version":		"0.1",
+		"description":	"This plugin allows to check user's network",
+		"author":		"Daniil Schetinskiy",
+		"help url":		"vk.com/norman74",
+		"category":		"General",
+		"type":			"object",
+		"rotatable":	true,
+		"dependency":	"offline.min.js",
+		"flags":		pf_singleglobal
+	};
+};
+
+////////////////////////////////////////
+// Conditions
+AddCondition(0, cf_none, "Is online", "Network connection", "Is network online", "Check if network connected", "isOnline");
+
+////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_number, "State", "Network connection", "state", "The current state of the connection 'up' or 'down'");
+
+////////////////////////////////////////
+ACESDone();
+var property_list = [];
+	
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance);
+}
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	this.instance = instance;
+	this.type = type;
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+IDEInstance.prototype.OnInserted = function(){}
+IDEInstance.prototype.OnDoubleClicked = function(){}
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+IDEInstance.prototype.OnRendererInit = function(renderer){}
+IDEInstance.prototype.Draw = function(renderer){}
+IDEInstance.prototype.OnRendererReleased = function(renderer){}
---
+++ runtime.js
@@ -0,0 +1,88 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.networkAddon = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.networkAddon.prototype;
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	var typeProto = pluginProto.Type.prototype;
+	typeProto.onCreate = function()
+	{
+		
+	};
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+		this.state = "";
+	};
+	var instanceProto = pluginProto.Instance.prototype;
+	instanceProto.onCreate = function()
+	{
+		var self = this;
+		Offline.on('confirmed-up', function(){
+			self.state = Offline.state;
+		}, self)
+		Offline.on('confirmed-down', function(){
+			self.state = Offline.state;
+		}, self)
+	};
+	instanceProto.onDestroy = function (){};
+	instanceProto.saveToJSON = function (){return {};};
+	instanceProto.loadFromJSON = function (o){};
+	instanceProto.draw = function(ctx){};
+	instanceProto.drawGL = function (glw){};
+	instanceProto.getDebuggerValues = function (propsections)
+	{
+		propsections.push({
+			"title": "Network",
+			"properties": [
+				{"name": "State", "value": this.state, "readonly": true}
+			]
+		});
+	};
+	instanceProto.onDebugValueEdited = function (header, name, value){};
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	
+	Cnds.prototype.isOnline = function ()
+	{
+		if (this.state == "up")
+			return true
+	};
+	
+	pluginProto.cnds = new Cnds();
+	
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	
+	pluginProto.acts = new Acts();
+	
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	
+	Exps.prototype.state = function (ret)
+	{
+		ret.set_string(this.state);
+	};
+	
+	pluginProto.exps = new Exps();
+
+}());
